a1a5ed36eccfe661ede1efd3a5c58594c870dc67
Beytullah Çetin <pasaport_07@hotmail.com>
2022-03-20 00:38:14 +0300

    rename ThirdPersonMovement.cs
    ThirdPersonMovement renamed to ThirdPersonAnimation

--- Assets/Scripts/ThirdPersonAnimation.cs
+++ Assets/Scripts/ThirdPersonAnimation.cs
@@ -3,46 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ThirdPersonMovement : MonoBehaviour
+public class ThirdPersonAnimation : MonoBehaviour
 {
     [SerializeField] Animator animator;
     Vector2 movementInput;
     Vector3 movement;
-    [SerializeField] float movementSpeed = 3f;
 
     private void Update()
     {
-        /* if (Keyboard.current.wKey.wasPressedThisFrame)
-        {
-            anim.SetBool("Walk", true);
-        }
-        else
-        {
-            anim.SetBool("Walk", false);
-        } */
-
-        Move(movement);
+        Animate(movement);
     }
 
-    public void OnMovementChanged(InputAction.CallbackContext context)
+    public void OnAnimationInput(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
         movement = new Vector3(movementInput.x, 0, movementInput.y);
-        Debug.Log(movement);
     }
 
-    void Move(Vector3 movement)
+    void Animate(Vector3 movement)
     {
-        //Change Position
-        transform.position += movement * movementSpeed * Time.deltaTime;
-
         //Play Animation
         animator.SetBool("Walk", movement.magnitude > 0 ? true : false);
         if (movement.z < 0)
             animator.SetFloat("WalkDirection", -1f);
         else
             animator.SetFloat("WalkDirection", 1f);
-
     }
 
 }
