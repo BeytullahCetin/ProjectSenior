c275fbb4a103f74a7edfdffdaad992a31b5fdf73
Beytullah Çetin <pasaport_07@hotmail.com>
2022-03-20 00:36:54 +0300

    create Movement.cs

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 movementInput;
+    Vector3 movement;
+    bool isRunning = false;
+
+    [Range(1,10)]
+    [SerializeField] float movementSpeed = 3f;
+    [Range(1.5f, 5f)]
+    [SerializeField]float runSpeedMultiplier = 2f;
+
+
+    void Update()
+    {
+        transform.Translate(movement * Time.deltaTime * (isRunning == false ? movementSpeed : movementSpeed * runSpeedMultiplier));
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log("OnMoveInput: " + context.phase + ": " + movementInput);
+        movement.x = movementInput.x;
+        movement.z = movementInput.y;
+    }
+
+    public void OnRunInput(InputAction.CallbackContext context)
+    {   
+        if(context.performed){
+            isRunning = !isRunning;
+            Debug.Log(isRunning);
+        }
+    }
+}
