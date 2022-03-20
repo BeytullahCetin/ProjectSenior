3d55bcd203202f7f50d967a5c943f5d76c32a317
Beytullah Çetin <pasaport_07@hotmail.com>
2022-03-21 00:13:32 +0300

    fix camera rotation
    fix 360 degree x-axis camera rotation

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class PlayerLook : MonoBehaviour
 {
     Vector2 lookInput;
-    float lookX;
-    float lookY;
-
+    
+    [Range(0,90)]
+    [SerializeField] float xClamp = 90f;
+    float xRotation;
+    Vector3 targetRotation;
 
     [SerializeField] Transform camTransform;
     [Range(1, 10)]
@@ -17,14 +19,19 @@ public class PlayerLook : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.up, lookX);
-        camTransform.Rotate(Vector3.right, lookY);
+        transform.Rotate(Vector3.up, lookInput.x);
+
+        xRotation -= lookInput.y;
+        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
+
+        targetRotation = transform.eulerAngles;
+        targetRotation.x = xRotation;
+        camTransform.eulerAngles = targetRotation;
+
     }
 
     public void OnLookInput(InputAction.CallbackContext context)
     {
-        lookInput = context.ReadValue<Vector2>() * lookSpeed;
-        lookX = lookInput.x;
-        lookY = -lookInput.y;
+        lookInput = context.ReadValue<Vector2>() * lookSpeed * Time.deltaTime;
     }
 }
