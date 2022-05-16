7b5d5ab676f13162a73f6da3734450e8ff18eb7c
Beytullah Çetin <pasaport_07@hotmail.com>
2022-05-16 23:34:01 +0300

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float throwForce = 1f;
     public float ThrowForce { get { return throwForce; } }
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+            return;
+
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        {
+            CollectibleController current = hit.collider.gameObject.GetComponent<CollectibleController>();
+            if (current != null)
+            {
+                current.Interaction();
+            }
+        }
+    }
 }
