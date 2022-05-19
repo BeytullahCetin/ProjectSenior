30c714b85e625840a46c1d446a4c40479f8ec450
Beytullah Çetin <pasaport_07@hotmail.com>
2022-05-19 14:22:06 +0300

    add light and sound to SoundLure

--- Assets/Scripts/Interactables/SoundLureController.cs
+++ Assets/Scripts/Interactables/SoundLureController.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class SoundLureController : LureController
 {
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] Light audioLight;
+    [SerializeField] Transform audioLightTransform;
+
     public override void Interaction()
     {
         if (canInteractable && !isUsed)
         {
-            Debug.Log("Interaction LureController");
+            audioLight.enabled = true;
+            audioSource.Play();
+            StartCoroutine(ChangeLightTransform());
+        }
+    }
+
+    IEnumerator ChangeLightTransform()
+    {
+        while (true)
+        {
+            audioLightTransform.Rotate(Vector3.right);
+            yield return null;
         }
     }
 
