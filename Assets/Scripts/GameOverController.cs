67e729b8122f8e430db452ce80cae95cf461112f
Beytullah Çetin <pasaport_07@hotmail.com>
2022-05-20 23:54:46 +0300

    Update GameOverController.cs

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOverController : MonoBehaviour
 {
+    [SerializeField] int delayAfterReload = 5;
     Canvas gameOverCanvas;
 
     void Reset()
@@ -33,10 +34,12 @@ public class GameOverController : MonoBehaviour
     {
         gameOverCanvas.enabled = true;
         GameObject.FindGameObjectWithTag("Player").SetActive(false);
+        StartCoroutine(ReloadScene());
     }
 
-    public void ReloadScene()
+    IEnumerator ReloadScene()
     {
+        yield return new WaitForSeconds(delayAfterReload);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
