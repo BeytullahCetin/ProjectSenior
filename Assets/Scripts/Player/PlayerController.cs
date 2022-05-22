7da16d40050bdeaf59678d1e3eecc6d3d36def0f
Beytullah Çetin <pasaport_07@hotmail.com>
2022-05-23 00:11:17 +0300

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Camera playerCamera;
     [SerializeField] float throwForce = 1f;
+
+    [SerializeField] float rayDistance  = 1f;
     InteractableController current;
     public float ThrowForce { get { return throwForce; } }
 
@@ -23,8 +25,8 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Raycast");
 
             RaycastHit hit;
-            Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * 10, Color.red, 1f);
-            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, 10))
+            Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * rayDistance, Color.red, 1f);
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, rayDistance))
             {
                 Debug.Log(hit.collider.name);
 
