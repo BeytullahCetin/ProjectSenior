37fee1d792432f7af82cde9c962889283dec19de
Beytullah Çetin <pasaport_07@hotmail.com>
2022-05-31 20:06:25 +0300

    add main ambience sound

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,7 +6,13 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
 
-    AudioSource audioSource;
+    [SerializeField] float fadeTime = .5f;
+    bool isPlayingMainAmbience = true;
+
+    [SerializeField] AudioSource mainAmbienceAudioSource;
+    [SerializeField] AudioSource externalAmbienceAudioSource;
+
+    [SerializeField] AudioSource oneShotAudioSource;
 
     private void Awake()
     {
@@ -16,13 +22,62 @@ public class SoundManager : MonoBehaviour
             Instance = this;
     }
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     public void PlayClip(AudioClip clip)
     {
-        audioSource.PlayOneShot(clip);
+        oneShotAudioSource.PlayOneShot(clip);
     }
+
+    public void PlayMainAmbience()
+    {
+        StopAllCoroutines();
+        StartCoroutine(PlayMainAmbienceRoutine());
+        isPlayingMainAmbience = true;
+    }
+
+    IEnumerator PlayMainAmbienceRoutine()
+    {
+        mainAmbienceAudioSource.Play();
+        float timeElapsed = 0;
+
+        while (timeElapsed < fadeTime)
+        {
+            mainAmbienceAudioSource.volume = Mathf.Lerp(0, 1, timeElapsed / fadeTime);
+            externalAmbienceAudioSource.volume = Mathf.Lerp(1, 0, timeElapsed / fadeTime);
+            timeElapsed += Time.deltaTime;
+        }
+
+        externalAmbienceAudioSource.Stop();
+        yield return null;
+    }
+
+    public void SwapAmbience(AudioClip clip)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeTrack(clip));
+        isPlayingMainAmbience = false;
+    }
+
+    IEnumerator FadeTrack(AudioClip clipToFadeIn)
+    {
+        float timeElapsed = 0;
+        Debug.Log("coroutine started");
+        if (true == isPlayingMainAmbience)
+        {
+            Debug.Log("coroutine if");
+            externalAmbienceAudioSource.clip = clipToFadeIn;
+            externalAmbienceAudioSource.Play();
+
+            while (timeElapsed < fadeTime)
+            {
+                externalAmbienceAudioSource.volume = Mathf.Lerp(0, 1, timeElapsed / fadeTime);
+                mainAmbienceAudioSource.volume = Mathf.Lerp(1, 0, timeElapsed / fadeTime);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            mainAmbienceAudioSource.Stop();
+        }
+    }
+
+
 }
